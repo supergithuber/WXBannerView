b5721e3cc8cf97cdff89e60d4079adac967eecb1
sivanwu <1984430988@qq.com>
2019-08-07 17:19:26 +0800

    update some

--- WXBannerView/BannerView/View/WXLoadImageView.h
+++ WXBannerView/BannerView/View/WXLoadImageView.h
@@ -10,6 +10,16 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// 下载进度回调
+typedef void (^WXDownloadProgressBlock)(unsigned long long total, unsigned long long current);
+/// 网络请求回调
+typedef void (^WXDownLoadDataBlock)(NSData * _Nullable data, NSError * _Nullable error);
+
+
+/// 下载完成回调, 为了imageView
+typedef void (^WXDownLoadImageBlock)(UIImage *image);
+
+
 @interface WXLoadImageView : UIImageView
 
 /// 指定URL下载图片失败时，重试的次数，默认为2次
--- WXBannerView/BannerView/View/WXLoadImageView.m
+++ WXBannerView/BannerView/View/WXLoadImageView.m
@@ -9,8 +9,49 @@
 #import "WXLoadImageView.h"
 #import <objc/runtime.h>
 
+/// MARK: - WXImageDownloader
+@interface WXImageDownloader : NSObject<NSURLSessionDownloadDelegate>
 
+@property (nonatomic, strong) NSURLSession *session;
+@property (nonatomic, strong) NSURLSessionDownloadTask *task;
+@property (nonatomic, assign) unsigned long long totalLength;
+@property (nonatomic, assign) unsigned long long currentLength;
+@property (nonatomic, copy) WXDownloadProgressBlock progressBlock;
+@property (nonatomic, copy) WXDownLoadDataBlock callbackOnFinished;
 
+/// 下载图片
+- (void)startDownloadImageWithUrl:(NSString *)url
+                         progress:(WXDownloadProgressBlock)progress
+                         complete:(WXDownLoadDataBlock)complete;
+
+@end
+
+@implementation WXImageDownloader
+
+- (void)startDownloadImageWithUrl:(NSString *)url
+                         progress:(WXDownloadProgressBlock)progress
+                         complete:(WXDownLoadDataBlock)complete{
+    self.progressBlock = progress;
+    self.callbackOnFinished = complete;
+    
+    if ([NSURL URLWithString:url] == nil) {
+        NSError *error = [NSError errorWithDomain:@"com.sivanwu.bannerView" code:400 userInfo:@{@"errorMessage": @"URL不正确"}];
+        complete(nil, error);
+        return;
+    }
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url] cachePolicy:NSURLRequestReturnCacheDataElseLoad timeoutInterval:60];
+    [request addValue:@"image/*" forHTTPHeaderField:@"Accept"];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    self.session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:queue];
+    NSURLSessionDownloadTask *task = [self.session downloadTaskWithRequest:request];
+    [task resume];
+    self.task = task;
+}
+@end
+
+/// MARK: - WXLoadImageView
 @implementation WXLoadImageView
 
 - (instancetype)initWithFrame:(CGRect)frame {
